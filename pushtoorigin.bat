e158462a6c9bb01b0a50f7f0eb0777bcadc447c2
alexdev404 <rexydogwoof01@gmail.com>
2021-05-04 11:53:42 -0600

    Updated Source

--- pushtoorigin.bat
+++ pushtoorigin.bat
@@ -1,17 +1,28 @@
 @echo off
 title PUSH + UPDATE SOURCE SCRIPT
 echo Setting Path...
+echo .
 set gitdir=C:\Users\User 1\Downloads\PortableGit
 set path=%gitdir%\cmd;%path%
 echo Adding Files...
+echo .
 git add .
+echo .
 echo Commiting Source...
+echo .
 git commit -m "Updated Source"
+echo .
 echo Adding Origin (if not already added)...
+echo .
 git remote add origin https://github.com/AlexDev404/misc-prog.git
+echo .
 echo Verifying Remote...
+echo .
 git remote -v
+echo .
 echo Pushing to Origin...
+echo .
 git push origin master
+echo .
 echo Done!
 pause
